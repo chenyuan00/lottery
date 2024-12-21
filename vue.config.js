d0f529ee10f58cf7831406f9d801fb40cbd87d15
chenyuan00 <2816823820@qq.com>
2024-12-21 23:28:43 +0800

    first commit

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/lottery' : '/'
+};
+
+// module.exports = {
+//   assetsDir: 'static',
+//   parallel: false,
+//   publicPath: './',
+// }
